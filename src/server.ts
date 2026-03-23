7bd1fcf6c134c1f65ecd1fe96bec51db7af26b67
Roberto Miranda Morales <rob.mirandam@gmail.com>
2026-03-22 19:08:51 -0600

    refactor: delete cors()

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import type { Application } from 'express';
-//import cors from 'cors'
 import users from './routes/users.ts'
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -10,15 +9,7 @@ const PORT = process.env.PORT || 3001;
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static(path.join(__dirname, '../dist')))
-// enable cors for all domains, only for dev stage
-// app.use(cors())
-// jusn in production
-// app.use(cors({
-//   origin: 'https://mi-sitio-web.com'
-// }));
-// this is use for body params in http post request
 app.use(express.json())
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
